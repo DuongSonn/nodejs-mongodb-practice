8611dbee40c99cd66ff0a200eb241ffc1a1e7392
Dương Trần Hoài Sơn <sondth@appota.com>
2022-01-12 14:32:46 +0700

    feat: init project

---
+++ config/default.ts
@@ -0,0 +1,4 @@
+export default {
+    port: 1998,
+    dbUri: 'mongodb://localhost:27017/nodejs-mongodb-practice',
+}
---
+++ src/utils/connect.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import config from 'config';
+import logger from './logger';
+
+async function connect() {
+    const dbUri = config.get<string>('dbUri');
+
+    try {
+        await mongoose.connect(dbUri);
+
+        logger.info('Connectd to DB');
+    } catch (error) {
+        logger.error('Error connect to DB: ' + error);
+        process.exit(1);
+    }
+}
+
+export default connect;
